2694d4f8e373e278980d2e744aabf27e64fbdf21
JeromeJaya <jeromeat2002@gmail.com>
2026-06-13 19:29:34 +0530

    modified api js file

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const VITE_API = import.meta.env.VITE_API || "http://localhost:5000";
+
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${VITE_API}/api`,
   withCredentials: true,
 });
